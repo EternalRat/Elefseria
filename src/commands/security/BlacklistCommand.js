dfa7c9401af13108499c50a83a6b8541a55bf06e
TrueBabyChaise <tomaugier23@gmail.com>
2021-11-27 22:28:55 +0100

    Push fixsecurity

--- src/commands/security/BlacklistCommand.js
+++ src/commands/security/BlacklistCommand.js
@@ -4,53 +4,53 @@ const blacklist = require('../../utils/database/models/blacklist');
 const { Client, Message } = require('discord.js');
 
 module.exports = class BlacklistCommand extends BaseCommand {
-  constructor() {
-    super('blacklist', 'security', [], 3, true, "Add an user to the blacklist", "<user/userId> <reason>", new PermissionGuard(["ADMINISTRATOR"]));
-  }
+	constructor() {
+		super('blacklist', 'security', [], 3, true, "Add an user to the blacklist", "<user/userId> <reason>", new PermissionGuard(["ADMINISTRATOR"]));
+	}
 
-  /**
-   * 
-   * @param {Client} client 
-   * @param {Message} message 
-   * @param {Array} args 
-   */
-  async run(client, message, args) {
-    let target = message.guild.members.cache.get(message.mentions.users.first()).id || message.guild.member(message.mentions.users.first()).id || args[0]
-    if (!target) return message.channel.send(`Usage: ${this.usage}`);
-    let reason = args.slice(1).join(" ");
-    if (!reason) return message.channel.send(`Usage: ${this.usage}`);
-    let findBan = await blacklist.findOne({ userId: target })
-    if (!findBan) {
-      let dbBanModel = new blacklist({
-        userId: target,
-        Reason: reason
-      })
-      dbBanModel.save().catch(err => console.log(err))
-      message.channel.send("This user has been added to the blacklist")
-    } else {
-      const filter = (reaction, user) => ["👍", "👎"].includes(reaction.emoji.name) && user.id === message.author.id
-      message.channel.send("This user is already in the blacklist, do you want to remove it ? Use the current reaction bellow.").then(message => {
-        message.react("👎")
-        message.react("👍")
-        message.awaitReactions(filter, {
-          max: 1,
-          time: 30000,
-          errors: ["time"]
-        }).then(async (collected) => {
-          const reaction = collected.first()
-          switch (reaction.emoji.name) {
-            case "👍":
-              blacklist.deleteOne(findBan, function(err) {
-                  if (err) console.log(err)
-              })
-              message.channel.send("This user has been removed from the blacklist.")
-              break
-            case "👎":
-              message.channel.send("Okay boss.")
-              break
-          }
-        })
-      })
-    }
-  }
+	/**
+	 * 
+	 * @param {Client} client 
+	 * @param {Message} message 
+	 * @param {Array} args 
+	 */
+	async run(client, message, args) {
+		let target = (await message.guild.members.fetch(message.mentions.users.first())).id || args[0]
+		if (!target) return message.channel.send(`Usage: ${this.usage}`);
+		let reason = args.slice(1).join(" ");
+		if (!reason) return message.channel.send(`Usage: ${this.usage}`);
+		let findBan = await blacklist.findOne({ userId: target })
+		if (!findBan) {
+			let dbBanModel = new blacklist({
+				userId: target,
+				Reason: reason
+			})
+			dbBanModel.save().catch(err => console.log(err))
+			message.channel.send("This user has been added to the blacklist")
+		} else {
+			const filter = (reaction, user) => ["👍", "👎"].includes(reaction.emoji.name) && user.id === message.author.id
+			message.channel.send("This user is already in the blacklist, do you want to remove it ? Use the current reaction bellow.").then(message => {
+				message.react("👎")
+				message.react("👍")
+				message.awaitReactions(filter, {
+					max: 1,
+					time: 30000,
+					errors: ["time"]
+				}).then(async (collected) => {
+					const reaction = collected.first()
+					switch (reaction.emoji.name) {
+						case "👍":
+							blacklist.deleteOne(findBan, function(err) {
+									if (err) console.log(err)
+							})
+							message.channel.send("This user has been removed from the blacklist.")
+							break
+						case "👎":
+							message.channel.send("Okay boss.")
+							break
+					}
+				})
+			})
+		}
+	}
 }
--- src/commands/security/VerifyCommand.js
+++ src/commands/security/VerifyCommand.js
@@ -4,25 +4,28 @@ const PermissionGuard = require('../../utils/PermissionGuard');
 const BaseCommand = require('../../utils/structures/BaseCommand');
 
 module.exports = class VerifyCommand extends BaseCommand {
-  constructor() {
-    super('verify', 'security', [], 60, true, "Check every member on the guild", null, new PermissionGuard(["ADMINISTRATOR"]));
-  }
+	constructor() {
+		super('verify', 'security', [], 60, true, "Check every member on the guild", null, new PermissionGuard(["ADMINISTRATOR"]));
+	}
 
-  /**
-   * 
-   * @param {Client} client 
-   * @param {Message} message 
-   * @param {Array} args 
-   */
-  async run(client, message, args) {
-    var guild = message.guild
-    let people = null
-    let members = await guild.members.fetch()
-    members.filter(member => !member.user.bot).forEach(async(member) => {
-      let findBan = await blacklist.findOne({userId: member.id})
-      if (findBan) people += `${member.user.username},`
-    });
-    if (people === null) return message.channel.send(`There is anyone in your server who are in the bdd. You're safe.`)
-    message.channel.send(`${people} are all the member who are in the bdd, be careful with them!`)
-  }
+	/**
+	 * 
+	 * @param {Client} client 
+	 * @param {Message} message 
+	 * @param {Array} args 
+	 */
+
+	async run(client, message, args) {
+		var guild = message.guild
+		let people = new Array()
+		let members = await guild.members.fetch()
+		members.filter(member => !member.user.bot).forEach(member => {
+			let findBan = blacklist.findOne({userId: member.id})
+			if (findBan) people.push(member.user.username)
+			console.log("Avant", people)
+		});
+		console.log("Apres", people)
+		if (people.length === 0) return message.channel.send(`No one on this server is blacklist in the bbd. You're safe.`)
+		message.channel.send(`${people} are all the member who are in the bdd, be careful with them!`)
+	}
 }
